c8a6e348571eea8bdc4a73dc43333779654fc944
tokifumi <t.takemoto@internous.co.jp>
2023-07-14 18:03:38 +0900

    catgptに聞いて改善

--- selectlinux.sh
+++ selectlinux.sh
@@ -3,25 +3,25 @@
 while true
 do
  echo "起動するLinuxを選択してください" 
- echo "c)CentOS"
- echo "u)Ubuntu"
- read ANS
+ echo "1)CentOS"
+ echo "2)Ubuntu"
+ read -p "1か2で選択してください： " ANS
 
  case $ANS in
-	c)
+	1)
 	echo -e "CentOSを起動します\n"
-	docker run --rm -it --name centos7 -h centos7 centos:7
+	docker run --rm -it --name centos7-$(date +%s) -h centos7 centos:7
 	break
 	;;
 
-	u)
+	2)
 	echo -e "Ubuntuを起動します\n"
-	docker run --rm -it --name ubuntu -h ubuntu ubuntu:latest
+	docker run --rm -it --name ubuntu-$(date +%s) -h ubuntu ubuntu:latest
 	break
 	;;
 
 	*)
-	echo -e "cかuで選択してください\n"
+	echo -e "無効な選択肢です\n"
 	continue
 	;;
  esac
